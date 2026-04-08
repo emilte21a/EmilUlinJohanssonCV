c32bb41e52efc428c129fed339b4f9371fe9396e
Emil <112473413+emilte21a@users.noreply.github.com>
2026-04-08 17:09:18 +0200

    uppdatering

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ function moveToSection(sectionId) {
     }
 }
 
+
+const copyButton = document.getElementById("copyButton");
+
+copyButton.addEventListener("click", (e) => {
+    const codeAsText = "emilulinjohansson.arbete@gmail.com";
+    navigator.clipboard.writeText(decodeURIComponent(codeAsText));
+});
+
