8b4ffb8de6c127d79105885da00c5a67c28180ce
Emil <112473413+emilte21a@users.noreply.github.com>
2026-09-13 22:50:01 +0200

    update!!!!

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// document.getElementById('aboutSection').addEventListener('click', function () {
-//     moveToSection('aboutSectionContent');
-// });
-// document.getElementById('projectSection').addEventListener('click', function () {
-//     moveToSection('projectSectionContent');
-// });
-
-
-function moveToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-        const sectionTop = section.getBoundingClientRect().top + window.scrollY - 200;
-        window.scrollTo({
-            top: sectionTop,
-            behavior: 'smooth'
-        });
-
-
-    } else {
-        console.error('Section not found:', sectionId);
-    }
-}
-
-
 const copyButton = document.getElementById("copyButton");
 
 copyButton.addEventListener("click", (e) => {
